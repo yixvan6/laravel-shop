e145b43a9a2c990efc55af6b2a85137666045d63
yixvan6 <yixvan6@163.com>
2019-01-07 09:23:06 +0000

    basic layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title', 'Welcome') - Laravel Shop</title>
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        <div class="{{ route_class() }}-page" id="app">
+            @include('layouts._header')
+            <div class="container">
+                @yield('content')
+            </div>
+            @include('layouts._footer')
+        </div>
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>

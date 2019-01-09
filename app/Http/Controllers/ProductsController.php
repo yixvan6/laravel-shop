464965dd7b365a07436f5248863fe9ab7ac7d834
yixvan6 <yixvan6@163.com>
2019-01-09 07:58:18 +0000

    products order & search

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,10 +7,39 @@ use App\Models\Product;
 
 class ProductsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::where('on_sale', true)->paginate(16);
+        $builder = Product::query()->where('on_sale', true);
 
-        return view('products.index', compact('products'));
+        // 若有搜索字段
+        if ($search = $request->search) {
+            $like = '%'.$search.'%';
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        // 若有排序参数
+        if ($order = $request->order) {
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+        // 过滤的参数
+        $filters = [
+            'order' => $order,
+            'search' => $search,
+        ];
+
+        return view('products.index', compact('products', 'filters'));
     }
 }
